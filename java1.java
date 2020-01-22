741266895e40306dd2659fcc2488100599a8ac84
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-01-22 16:37:34 +0530

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,24 @@
+public class Largest
+{
+   public static void main(String args[])
+   {
+        int a,b,c;   //local variable
+     a=Integer.parseInt(args[0]);
+     b=Integer.parseInt(args[1]);
+     c=Integer.parseInt(args[2]);
+       
+      if(a>b&&a>c)     //compare variable with two other variable
+       {
+    	 System.out.println(a+ "largest number");     //print largest number
+
+       }
+       else if(b>a&&b>c)
+        {
+          System.out.println(b+ "largest number");
+         }
+        
+      else
+        {System.out.println(c+ "largest number");
+        }
+}
+}
