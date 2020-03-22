032e321d5372848a940feefbf939bb6a7e0ad002
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-03-22 22:12:53 +0530

    Create Outer.java
    static nested class example with instance method

---
+++ Outer.java
@@ -0,0 +1,19 @@
+class Outer
+{
+    static String name="rajat";
+      static class Inner
+          {
+              void msg()
+                {
+                     System.out.println("Name is:" +name);
+                }
+           } 
+     public static void main(String[] args)
+        {
+           Outer.Inner obj = new Outer.Inner(); 
+            obj.msg();
+         }
+}
+
+//dont need to be created the object of outer class because nested class is static 
+and static properties,methods or classes can be accessed with out object 
