a7d8e3fb488b778f96419266518b9b8db4b819cb
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-03-22 22:16:48 +0530

    Update Outer.java
    add comment

--- Outer.java
+++ Outer.java
@@ -10,10 +10,10 @@ class Outer
            } 
      public static void main(String[] args)
         {
-           Outer.Inner obj = new Outer.Inner(); 
+           Outer.Inner obj = new Outer.Inner();   //no need to create Outer class object
             obj.msg();
          }
 }
 
 //dont need to be created the object of outer class because nested class is static 
-and static properties,methods or classes can be accessed with out object 
+//and static properties,methods or classes can be accessed with out object 
