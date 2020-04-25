6e0f6fbf895c471c903d1346559fe1ee77ddbef7
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-04-26 01:54:08 +0530

    print Current date

---
+++ Exp11.1/Servlet/DateServlet.java
@@ -0,0 +1,23 @@
+package pkg1;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class DateServlet extends HttpServlet{
+
+public void doGet(HttpServletRequest request, HttpServletResponse
+response) throws ServletException, IOException{
+
+PrintWriter pw = response.getWriter();
+
+Date today = new Date();
+
+pw.println("<html>"+"<body bgcolor=\"#999966\"></html>");
+
+//<h1>Date and Time with Servlet</h1>
+pw.println("<b>"+ today+"</b></body>"+ "</html>");
+
+}
+}
