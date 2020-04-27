e4529e896dfd5b3fddde71bbf2efaf6ffcb6bd42
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-04-28 00:43:43 +0530

    Create Buffer.java

---
+++ Exp8/Buffer.java
@@ -0,0 +1,24 @@
+public class Buffer
+{
+    public static String toUpperCase(StringBuffer s)
+{
+    String str = "";
+    for(int x=0; x<s.length(); x++){
+        char ch = s.charAt(x);
+        if(ch >= 'a' && ch <= 'z')
+            str += "" + (char)(ch - 32);        
+        else
+            str += "" + ch; 
+    }
+    return str;
+}
+
+public static void main(String[] args)
+{
+    System.out.println(toUpperCase(new StringBuffer("rajat Panwar")));
+        System.out.println(toUpperCase(new StringBuffer("RithIK ChoUdhary")));
+      System.out.println(toUpperCase(new StringBuffer("Harshil Bhardwaj")));
+      System.out.println(toUpperCase(new StringBuffer("nipun singal")));
+       System.out.println(toUpperCase(new StringBuffer("Rakshit KaPoOr")));  
+}
+}
