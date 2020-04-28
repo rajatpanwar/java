4a071a8f8be74aae66d2a3eddbc31c42b374fce4
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-04-28 23:53:39 +0530

    after comment for try block

--- Exp7/Ques2/Main.java
+++ Exp7/Ques2/Main.java
@@ -11,7 +11,7 @@ class Excep
          b=input.nextInt();
           try
           {
-             c=a/b;
+             c=a/b;                                         //throw try block exception
           }
           catch(ArithmeticException e)
             {
