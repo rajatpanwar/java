ec52b82d8fe3d51dbe976258724d8f8b14351d84
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-02 01:14:46 +0530

    after comment

--- Exp6/que2/Account1.java
+++ Exp6/que2/Account1.java
@@ -1,4 +1,4 @@
-import Balance.Account;
+import Balance.Account;                        //we can call the class with the help of import package from Account.java class to Account1.java
 public class Account1
 {
    public static void main(String[] args)
