0dac7768f5bf90cd31de5a591bb684c2197179d1
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-02 01:13:08 +0530

    create a Account1.java file

---
+++ Exp6/que2/Account1.java
@@ -0,0 +1,9 @@
+import Balance.Account;
+public class Account1
+{
+   public static void main(String[] args)
+     {
+        Account obj=new Account();
+         obj.Display_Balance();
+       }
+}
