b06615e0759204041deee12d45da507e1922c7a3
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-04 13:34:19 +0530

    using priority and synchronization

---
+++ Exp9/Main3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class que4 implements Runnable
+{
+  public synchronized void run() 
+  {
+   System.out.println(Thread.currentThread().getName());
+   System.out.println("Threading is done"+Thread.currentThread().getPriority());
+	}
+}
+public class Main3 {
+
+   public static void main(String[] args) throws InterruptedException {
+    que4 m=new que4();
+    Thread t1=new Thread(m);
+    Thread t2=new Thread(m);
+    Thread t3=new Thread(m);
+    Thread t4=new Thread(m);
+    Thread t5=new Thread(m);
+    t1.start();
+    t1.setPriority(7);  //setting priority
+    t2.start();
+    t2.setPriority(3);
+    t3.start();
+    t3.setPriority(8);
+    t4.start();
+	t4.setPriority(1);
+	t5.start();
+	t5.setPriority(6);
+/* t1.setPriority(7);
+t2.setPriority(3);
+t3.setPriority(8);
+t4.setPriority(1);
+t5.setPriority(6); */
+System.out.println(t1.isAlive()); // checking whether the thread is alive or not
+System.out.println(t2.isAlive());
+	}
+
+}
