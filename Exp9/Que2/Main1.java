dcbf92a4f7274ebed4c2ed4ba505eb3654fbdf1a
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-04 13:27:22 +0530

    Create Main1.java

---
+++ Exp9/Que2/Main1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Multi extends Thread
+{
+        public void run()
+        { 
+           for(int i=0;i<10;i++)
+              {
+                 if(i % 2 == 0)
+                  {
+                     System.out.println("even number: " +i);
+                   }
+               }
+          }
+}
+
+class Multi1 extends Thread
+{
+     public void run()
+     {
+        for(int i=0;i<10;i++)
+          {
+              if(i % 2 == 1)
+                {
+                  System.out.println("odd number: " +i);
+                 }
+          }
+      }
+}
+
+public class Main1
+{
+    public static void main(String[] args)
+       {
+            Multi t=new Multi();
+            t.start();
+            Multi1 t1=new Multi1();
+             t1.start();
+         }
+}
+              
