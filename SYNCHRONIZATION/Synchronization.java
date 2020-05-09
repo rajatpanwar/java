9fbb1648a2d44bb4d943e5ec3ab081d7a1e6a3ed
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-09 11:25:58 +0530

    Synchronization method

---
+++ SYNCHRONIZATION/Synchronization.java
@@ -0,0 +1,74 @@
+import java.util.*;
+class Table 
+{
+  synchronized void printTable(int n)    //Synchronization method
+{
+    
+   int i;
+   for(i=1;i<=5;i++)
+
+   {System.out.println(n*i);
+    try {
+          Thread.sleep(400);
+          }
+      catch(Exception e)
+         
+           {System.out.println(e);}
+}
+}
+}
+class MThread extends Thread
+{
+    Table t;
+ MThread(Table t)
+{
+    this.t=t;
+}
+public void run()
+{
+  t.printTable(5);
+}
+}
+
+class MThread1 extends Thread
+{
+     Table t;
+   MThread1(Table t)
+     {
+          this.t=t;
+      }
+
+public void run()
+{
+   t.printTable(100);
+}
+}
+
+class Synchronization1
+{
+    public static void main(String args[])
+      {
+         Table obj=new Table();            //create object of class
+          MThread t1=new MThread(obj);
+          MThread1 t2=new MThread1(obj);
+           t1.start();
+           t2.start();
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
