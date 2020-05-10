41117c3fc005c7f056a11f6fc6334aa735a279b3
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-10 17:17:06 +0530

    return the index

---
+++ Index/Index.java
@@ -0,0 +1,10 @@
+
+public class Index
+{   
+         public static void main(String[] args)
+          {
+            String s1=new String("rajat");        //create a object
+             int i=s1.indexOf('j');
+              System.out.println("index is" +i);
+       }
+}
