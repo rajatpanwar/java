8cccbd0ebca1a93c91a79f51d6caa8ffa89f3224
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-10 17:18:06 +0530

    after second commit

--- Index/Index.java
+++ Index/Index.java
@@ -4,7 +4,7 @@ public class Index
          public static void main(String[] args)
           {
             String s1=new String("rajat");        //create a object
-             int i=s1.indexOf('j');
+             int i=s1.indexOf('j');                   //retuen the index of given eelement
               System.out.println("index is" +i);
        }
 }
