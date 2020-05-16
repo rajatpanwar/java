cf274c3a7c6eed7dca984d12772d9e226db8096a
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-17 00:28:15 +0530

    print all natural number sum

---
+++ LOOp/Natural.java
@@ -0,0 +1,11 @@
+public class Natural
+{
+     public static void main(String[] args)
+       {   int sum=0;
+         for(int i=0;i<10;i++)
+          {
+           sum=sum+i;
+           }
+           System.out.println("Sum is :- "+sum);
+        }
+} 
