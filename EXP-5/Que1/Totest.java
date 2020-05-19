5bb6bcd86a16c052cbd8f1fbe1f307072442db67
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-19 22:50:54 +0530

    Ques 1

---
+++ EXP-5/Que1/Totest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+interface test                //create interface
+{
+	void square(int num);
+}
+ class arthmatic implements test
+{
+	public void square(int num)
+	{
+		int b=num*num;
+		System.out.println(b);
+	}
+}
+ public class Totest 
+{
+		public static void main(String[] args) 
+		{
+			System.out.println("enter the number to be sqared\n");
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+			
+			arthmatic s=new arthmatic();
+			s.square(a);
+			sc.close();
+		}
+
+}
