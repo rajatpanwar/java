e3baa8aee03311967becfed2e695e2dedda9b897
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-19 22:56:54 +0530

    load main function

---
+++ account.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ public class account { //make a super class account
+   int balance =500;//declare min balance
+   int rate=4;//declare intrest rate for compound intrest
+   
+    public static void main(String[] args) {
+        sav_acct obj=new sav_acct();// make object of saving account
+        cur_acct obj1=new cur_acct();//make object of current account
+        
+     Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name");
+      String customer_name =sc.nextLine();
+        System.out.println("enter your account number");
+      int account_number =sc.nextInt();
+        System.out.println("enter type of account");
+        System.out.println("types S for saving account and C for current account");
+        char type=sc.next().charAt(0);
+        boolean q=true;//condition to return back to the menu
+        while(q){
+        if(type=='S'){
+            
+            System.out.println("1.deposit");
+            System.out.println("2.withdraw");
+            System.out.println("3.compound intrest");
+            System.out.println("4.EXIT");
+            System.out.println("enter any no. to perform the task");
+            int n=sc.nextInt();
