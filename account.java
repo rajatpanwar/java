a6b8acec90ba8aafae0ad9082b76bced1eb3bf41
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-19 22:58:13 +0530

    define all function

--- account.java
+++ account.java
@@ -52,4 +52,54 @@ import java.util.Scanner;
                 System.exit(0);
             }
           } } } }
+   class cur_acct extends account{ //current account class
+        public void  display(){
+            if(balance<500){
+                System.out.println("not enough money deposit the minimum balance");
+            }
+            else{
+                System.out.println("your balance is Rs."+ balance);
+            }
+            
+        }
+    }
+    class sav_acct extends account{// saving acount class 
+         Scanner sc1=new Scanner(System.in);
+        public void deposit(){
+            
+            System.out.println("enter the amount you want to deposit");
+            int amt=sc1.nextInt();
+            balance=balance+amt;
+            System.out.println("Rs."+ amt +" have been deposited to your account");
+            System.out.println("updated balance is Rs." +balance);
+            
+            
+       }
+         public void withdraw(){
+            System.out.println("enter the amount you want to withdraw");
+            int amt1=sc1.nextInt();
+            if(balance<500){
+                System.out.println("not sufficient balance deposit the min balance");
+            }
+            else{
+            balance=balance-amt1;
+            System.out.println( "Rs."+ amt1 +" have been withdrawn from your account ");
+             System.out.println("updated balance is Rs."+balance);
+        }}
+        public void  compound(){
+            int r=1;
+            System.out.println("enter the no. of years for intrest applied");
+            int t=sc1.nextInt();
+            for(int i=1;i<=t;i++){
+                r=r*(1+rate);
+                 
+            }
+            int compound_intrest=(balance*r)-balance;
+            System.out.println("your balance after the intrest is Rs."+ compound_intrest);
+            
+        }
+        
+    }
+   
+
    
