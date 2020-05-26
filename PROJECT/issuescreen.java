975634d4e8719b52a07a541dd3da3dacd0f4de1f
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-26 23:15:05 +0530

    implement interface and define variable

---
+++ PROJECT/issuescreen.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+import java.text.*;
+import java.util.Date;
+
+public class issuescreen extends JFrame implements ActionListener
+{
+
+ JLabel l1,l2,l3,l4,l5,l6,l7;
+  JTextField f2,f3,f4,f5,f6;
+ JComboBox b1;
+ JButton a1,a2,a3;
