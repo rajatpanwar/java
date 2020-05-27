39fc5b912ce4ab89c01e1acb64fa75286c4447a7
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-27 13:48:17 +0530

    define main function and try block

--- PROJECT/tables.java
+++ PROJECT/tables.java
@@ -20,4 +20,35 @@ public class tables extends JFrame
     for(int j=0;j<4;j++)
    { tm.setValueAt(head[j],0,j);}
     p1= new JScrollPane
+    (t1,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    add(p1);
+   
+    
+ try
+   {Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+    Connection con=DriverManager.getConnection("jdbc:odbc:inventorydsn");
+    Statement Stmt=con.createStatement();
+    String query="select * from TBLissue ";
+    ResultSet rs= Stmt.executeQuery(query);
+    while(rs.next())
+    { String s[]={String.valueOf(rs.getInt(1)),rs.getString(2),rs.getString(3),String.valueOf(rs.getString(4))};
+     tm.addRow(s);
+    }
+    con.close();
+   }
+
+  catch(Exception e)
+  {System.out.println("exception catched");}
+
+}  
+
+public static void main(String args[])
+ {tables l=new tables();
+  l.setVisible(true);
+  l.setSize(800,800);
+  l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+ }
+
+}
+
     
