5dd595e387a06ca24a597434a5595d1e9001550b
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-27 13:50:07 +0530

    import package and implement interface

---
+++ PROJECT/stockupdate.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class stockupdate extends JFrame implements ActionListener
+{
+
+ JLabel l1,l2,l3,l4,l5;
+  JTextField f2,f3,f4;
+ JComboBox b1;
+ JButton a1,a2,a3,a4;
