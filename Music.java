fe396526ed825553389a6e86e8dd6f905e0c4c68
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-28 21:08:24 +0530

    using interface

---
+++ Music.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+interface sales
+{
+	int sale();
+}
+class publication implements sales
+{
+	Scanner sc=new Scanner(System.in);
+	publication()
+	{
+	}
+	void show()
+	{
+		boolean t=true;
+		while(t==true)
+		{
+		System.out.println("Select among (1)book , (2)tape , (3)Exit");
+		int option=sc.nextInt();
+		if(option == 1)
+		{
+			book obj=new book();
+			System.out.println("1>add books ,2>display books");
+			int opt=sc.nextInt();
+			if(opt==1)
+			{
+				obj.jok();
+			}
+			else if(opt==2)
+			{
+				obj.display();
+			}
+			else
+			{
+				System.out.println("Wrong input");
+			}
+			
+		}
+		else if(option == 2)
+		{
+			tape obj2=new tape();
+			System.out.println("1>add tape ,2>Display tape");
+			int ko=sc.nextInt();
+			if(ko==1)
+			{
+				obj2.moi();
+				
+			}
+			else if(ko==2)
+			{
+				obj2.display();
+			}
+			else
+			{
+				System.out.println("Wrong input");
+			}
+		}
+		else if(option==3)
+		{
+			t=false;
+		}
+		else
+		{
+			System.out.println("Wrong input");
+		}
+		}
+	}
