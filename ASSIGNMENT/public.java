0aca84f867af7fc1420914404dd56754a8f37002
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-29 23:15:50 +0530

    extends publications class

--- ASSIGNMENT/public.java
+++ ASSIGNMENT/public.java
@@ -22,3 +22,55 @@ class publication implements sales
 		}
 	}
 	
+void page()
+	{
+		System.out.println("Enter the number of page in book");
+		int pagno=in.nextInt();
+		return pagno;
+	}
+	void display()
+	{
+		for(int j=0;j<number;j++)
+		{
+			System.out.println("title="+titl[j]+ "price ="+pri[j]+" page="+pag[j]);
+		}
+	}
+	~book();
+}
+class tape extends publication
+{
+	Scanner s=new Scanner(System.in);
+	String[] titt=new String[10];
+	float[] pre=new float[10];	
+	float time=new float[10];
+	int num=0;	
+	tape()
+	{
+		num++;
+		for(int k=0;k<num;k++)
+		{
+		System.out.println("Enter the title of tape");
+		titt[k]=title();
+		System.out.println("Enter the price of tape");
+		pre[k]=price();
+		System.out.println("Enter the play time");
+		time[k]=playing();
+		}
+	}
+	void playing()
+	{
+		float tim=s.nextFlaot();
+		return tim;
+	}
+	void display()
+	{
+		for(int y=0;y<number;y++)
+		{
+			System.out.println("title="+titt[k]+ "price ="+pre[k]+" playing time="+time[y]);
+			
+		}
+	}
+	~tape();
+}
+public class music
+{
