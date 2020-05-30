de4a74ee039dbf1d024170e2bee1c2bffa30c1b9
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-05-30 22:36:05 +0530

    Lshift

--- BASIC/OPERATOR/Lshift.java
+++ BASIC/OPERATOR/Lshift.java
@@ -1,5 +1,7 @@
-class Lshift{  
-public static void main(String args[]){  
+class Lshift
+{  
+public static void main(String args[])
+{  
 System.out.println(10<<2);//10*2^2=10*4=40  
 System.out.println(10<<3);//10*2^3=10*8=80  
 System.out.println(20<<2);//20*2^2=20*4=80  
