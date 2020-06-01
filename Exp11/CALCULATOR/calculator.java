b5c6a7789940c6776b9a02cae04f67d7d63d290a
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-01 22:56:41 +0530

    third commit

--- Exp11/CALCULATOR/calculator.java
+++ Exp11/CALCULATOR/calculator.java
@@ -7,10 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Calculator extends HttpServlet {
+public class Calculator extends HttpServlet 
+{
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException 
+            {
                
                 PrintWriter out = response.getWriter();
                 String n1 = request.getParameter("txt1");
