8ffaa38a907725477c1a6937b5ecaf870e480614
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-01 23:09:59 +0530

    Update First.java

--- Exp8/First.java
+++ Exp8/First.java
@@ -3,20 +3,19 @@ import java.util.*;
 
 public class First
 {
-    
     public static void main(String[] args)
        {
-             String str="I am Rajat Panwar";
+              String str="I am Rajat Panwar";
 
-           int firstocc = str.indexOf('a');                            
-            System.out.println("First occurrence of 'a' is" +firstocc);
+              int firstocc = str.indexOf('a');                            
+              System.out.println("First occurrence of 'a' is" +firstocc);
 
            
-            int lastocc = str.lastIndexOf('a');
-             System.out.println("last occurrence of 'a' is " +lastocc);
+              int lastocc = str.lastIndexOf('a');
+              System.out.println("last occurrence of 'a' is " +lastocc);
            
                
-             int lastin = str.lastIndexOf("am");
+              int lastin = str.lastIndexOf("am");
               System.out.println("index  of last substring 'am' is:- "+lastin);
         }
 }
