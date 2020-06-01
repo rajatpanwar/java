c03051beca48743ca7078923e9bd503f0726a0cb
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-01 23:11:30 +0530

    Update Name.java

--- Exp8/Name.java
+++ Exp8/Name.java
@@ -1,15 +1,16 @@
 public class Name
 {
    public static void main(String[] args)
-{     int j;
+   {    
+       int j;
        int incr=0;
-   String n="Rajat Panwar";
-   System.out.println("Name is:---"+n);
-    System.out.println("String length is:- "+n.length());     //find the length of given string
-     for(j=0;j<n.length();j++)
-       {
-           if(n.charAt(j)=='a' || n.charAt(j)=='a')             
-              {
+       String n="Rajat Panwar";
+       System.out.println("Name is:---"+n);
+       System.out.println("String length is:- "+n.length());     //find the length of given string
+        for(j=0;j<n.length();j++)
+          {
+             if(n.charAt(j)=='a' || n.charAt(j)=='a')             
+               {
                  incr++;
                  System.out.println("position is of element a:-"+j);
                  System.out.println("element is found"+incr+"times");
