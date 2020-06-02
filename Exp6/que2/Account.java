96230655894469f062882f37ac6dc8a3c3e8f910
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-02 22:39:01 +0530

    after commit

--- Exp6/que2/Account.java
+++ Exp6/que2/Account.java
@@ -2,18 +2,18 @@ package Balance;
 import java.util.Scanner;
  public class Account
 {          
-          int salary,incr_sala,percentage,S;
+     int salary,incr_sala,percentage,S;
      public void Display_Balance()
         {
           Scanner sc=new Scanner(System.in);
-           System.out.println("Enter the salary");
-            salary=sc.nextInt();   
-            System.out.println("enter the percentage percentage ");
-            percentage=sc.nextInt();                                //take the input from user int type
-             S=(salary*percentage)/100; 
-           System.out.println(" percent is :"+S);
-           incr_sala=salary+S;
-            System.out.println("increment salary is :- "+incr_sala);
+          System.out.println("Enter the salary");
+          salary=sc.nextInt();   
+          System.out.println("enter the percentage percentage ");
+          percentage=sc.nextInt();                                //take the input from user int type
+          S=(salary*percentage)/100; 
+          System.out.println(" percent is :"+S);
+          incr_sala=salary+S;
+          System.out.println("increment salary is :- "+incr_sala);
         }
   
 }
