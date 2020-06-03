8654b771420d3b5b2776052017852aed9789976b
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-03 22:53:40 +0530

    main

--- Exp9/Que1/Main.java
+++ Exp9/Que1/Main.java
@@ -2,21 +2,20 @@ import java.util.*;
 class Multi extends Thread
 {
     public void run()
-  {     
-     for(int i=0;i<5;i++)
+    {     
+      for(int i=0;i<5;i++)
       {
-    System.out.println("Rajat....");
+        System.out.println("Rajat....");
       }
-   }
+     }
 }
 class Demo
 {
       Demo()
        {
-         Multi obj1=new Multi();
+          Multi obj1=new Multi();
           obj1.start();
-
-         }
+       }
 }
 
  class Multi1 implements Runnable                       //Runnable interface
@@ -32,7 +31,7 @@ class Threadrunnable
         {
            Multi1 m=new Multi1();
            Thread t1=new Thread(m);
-             t1.start();
+           t1.start();
          }
 }
 
