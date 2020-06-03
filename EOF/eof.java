e6f140016747bd1edddbfcb43e279530af094489
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-03 22:45:33 +0530

    after commit

--- EOF/eof.java
+++ EOF/eof.java
@@ -4,9 +4,11 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Solution
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc=new Scanner(System.in);
         int i=1;
         while (sc.hasNext())
