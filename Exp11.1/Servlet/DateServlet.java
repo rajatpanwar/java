82c8e48d7a15e71fce70245e7e4aef6546f15a22
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-03 22:48:14 +0530

    third commit

--- Exp11.1/Servlet/DateServlet.java
+++ Exp11.1/Servlet/DateServlet.java
@@ -5,19 +5,21 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class DateServlet extends HttpServlet{
+public class DateServlet extends HttpServlet
+{
 
-public void doGet(HttpServletRequest request, HttpServletResponse
-response) throws ServletException, IOException{
+  public void doGet(HttpServletRequest request, HttpServletResponse
+  response) throws ServletException, IOException
+  {
 
-PrintWriter pw = response.getWriter();
+    PrintWriter pw = response.getWriter();
 
-Date today = new Date();
+    Date today = new Date();
 
-pw.println("<html>"+"<body bgcolor=\"#999966\"></html>");    //we can make a colored  background with the help of this line
+     pw.println("<html>"+"<body bgcolor=\"#999966\"></html>");    //we can make a colored  background with the help of this line
 
-//<h1>Date and Time with Servlet</h1>
-pw.println("<b>"+ today+"</b></body>"+ "</html>");    //  print current date and time
+     //<h1>Date and Time with Servlet</h1>
+     pw.println("<b>"+ today+"</b></body>"+ "</html>");    //  print current date and time
 
-}
+   }
 }
