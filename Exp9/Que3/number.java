a2db885baf3bcf8196c42aa43293ad4d628152f6
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-03 22:56:01 +0530

    third commit

--- Exp9/Que3/number.java
+++ Exp9/Que3/number.java
@@ -1,38 +1,38 @@
  public class number
 {
-public static void main(String arg[])throws Exception
-{
-data d1=new data();
-data d2=new data();
-data d3=new data();
-data d4=new data();
-data d5=new data();
-data d6=new data();
-data d7=new data();
-data d8=new data();
-data d9=new data();
-data d10=new data();
-System.out.println(d9.count);
-}
+  public static void main(String arg[])throws Exception
+ {
+    data d1=new data();
+    data d2=new data();
+    data d3=new data();
+    data d4=new data();
+    data d5=new data();
+    data d6=new data();
+    data d7=new data();
+    data d8=new data();
+    data d9=new data();
+    data d10=new data();
+    System.out.println(d9.count);
+ }
 }
 class item { static int count=0; }
 class data extends item implements Runnable
 {
-item d=this;
-Thread t;
-data()
-{
-t=new Thread(this);
-t.start();
-}
-public void run()                                                    
-{ d=syn.increment(d); }
+  item d=this;
+  Thread t;
+  data()
+  {
+   t=new Thread(this);
+   t.start();
+  }
+  public void run()                                                    
+  { d=syn.increment(d); }
 }
 class syn
 {
-synchronized static item increment(item i)                                    //using synchronization
-{
-i.count++;
-return(i);
-}
+  synchronized static item increment(item i)                                    //using synchronization
+  {
+   i.count++;
+   return(i);
+  }
 }
