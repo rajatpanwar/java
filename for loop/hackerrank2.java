84f666c15c7b69237d0d35de69f1980671637fc7
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-04 22:24:35 +0530

    hackerrank

--- for loop/hackerrank2.java
+++ for loop/hackerrank2.java
@@ -6,23 +6,19 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
-
-
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int N = scanner.nextInt();
+public class Solution 
+{
+   private static final Scanner scanner = new Scanner(System.in);
+   public static void main(String[] args) 
+   {
+        int N = scanner.nextInt();                             
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-           int i,result,a;
+        int i,result,a;
 
-        for(i=1;i<=10;i++)
+        for(i=1;i<=10;i++)                                //for loop and printing
         {
-
             result=N*i;
-             
-             System.out.println(N+ " x "+i+" = "+i*N);
-        
-        
-        }  
+            System.out.println(N+ " x "+i+" = "+i*N);
+        }
+    }
+}
