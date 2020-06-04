bbf04c885abfe69556a37e7112fed8ec228b8b64
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-04 22:16:16 +0530

    after comment

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -2,14 +2,13 @@ public class Fib
 { 
      public static void main(String args[])
      {
-  	int a=0,b=1,c=10;
-           for(int j=0;j<=10;j++)
-              {c=a+b;
- 		
+  	int a=0,b=1,c=10;        //declare variables
+        for(int j=0;j<=10;j++)   //for loop to print fibonacci series
+        {
+          c=a+b;
  	  System.out.println(c);
-			
-			a=b;
-			b=c;
+        	a=b;
+		b=c;
 		}
 	}
 }
