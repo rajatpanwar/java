09dc2704c737a6f4d88f196d99dc373b8037ffbc
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-04 22:13:41 +0530

    after comment

--- Squarelab/Square.java
+++ Squarelab/Square.java
@@ -3,9 +3,7 @@ public class Square1
 {
     public static int SquareSum(int a[]) 
     {
-        
         int sum=0;
-        
         int square=0;
                                   //for loop to square and sum of elements
         for(int i=0;i<10;i++)
@@ -29,7 +27,7 @@ public class Square1
                                              //take array element
         System.out.println("enter element");
         for(int i=0;i<10;i++)
-              arr[i]=sc.nextInt();
+        arr[i]=sc.nextInt();
                                                //print sum from calling the fuction SquareSum
         System.out.print("square sum of element "+SquareSum(arr));
      }
