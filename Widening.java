81f791020518a65fc9a0dd75ccf204c36abda23c
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-05 22:08:48 +0530

    second commit

--- Widening.java
+++ Widening.java
@@ -1,9 +1,10 @@
-class Widening{
-public static void main(String[] args)
+class Widening
 {
-int a=50;
-float f=a;
-System.out.println(a);
-System.out.println(f);
-}
+  public static void main(String[] args)
+  {
+   int a=50;
+   float f=a;
+   System.out.println(a);
+   System.out.println(f);
+  }
 }
