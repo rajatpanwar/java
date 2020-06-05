9757db1f46d663d5210680af2e64f24c4ea8ecba
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-05 22:10:40 +0530

    after comment

--- Narrowing.java
+++ Narrowing.java
@@ -2,7 +2,7 @@ class Narrowing
 {
   public static void main(String[] args)
   {
-   float f=20.3f;
+   float f=20.3f;          //narrowing will shorten the float variable into integer variable
    int a=(int)f;
    System.out.println(f);
    System.out.println(a);
