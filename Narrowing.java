73ae9298ca78e146b8e214d77255c566203907d8
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-05 22:07:34 +0530

    second commit

--- Narrowing.java
+++ Narrowing.java
@@ -1,9 +1,10 @@
-class Narrowing{
-public static void main(String[] args)
+class Narrowing
 {
-float f=20.3f;
-int a=(int)f;
-System.out.println(f);
-System.out.println(a);
-}
+  public static void main(String[] args)
+  {
+   float f=20.3f;
+   int a=(int)f;
+   System.out.println(f);
+   System.out.println(a);
+  }
 }
