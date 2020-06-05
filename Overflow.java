dcda12185d9c74ed866473b9d4db9b71c2b9a528
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-05 22:15:33 +0530

    second commit

--- Overflow.java
+++ Overflow.java
@@ -1,8 +1,10 @@
-class Overflow{
-public static void main(String[] args){
-int a=200;
-byte b=(byte) a;
-System.out.println(a);
-System.out.println(b);
-}
+class Overflow
+{
+  public static void main(String[] args)
+  {
+   int a=200;
+   byte b=(byte) a;
+   System.out.println(a);
+   System.out.println(b);
+  }
 }
