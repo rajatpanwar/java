4459883e4e19d524b8939f5c1a0ef7cd2904bb9c
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-06 22:55:23 +0530

    after comment

--- Index/Index.java
+++ Index/Index.java
@@ -3,8 +3,8 @@ public class Index
 {   
          public static void main(String[] args)
           {
-            String s1=new String("rajat");        //create a object
-             int i=s1.indexOf('j');                   //retuen the index of given eelement
-              System.out.println("index is" +i);
+            String s1=new String("rajat");           //create an object
+            int i=s1.indexOf('j');                   //return the index of given element
+            System.out.println("index is" +i);       //print the index
        }
 }
