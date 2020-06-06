ad7cdb5b3db24c379dda646dbda547120da2e7ad
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-06 22:50:15 +0530

    after comment

--- simplesumarray/sumarray.java
+++ simplesumarray/sumarray.java
@@ -4,12 +4,11 @@ import java.text.*;
 import java.util.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Solution 
+{                                        //simplearray function
 
-    /*
-     * Complete the simpleArraySum function below.
-     */
-    static int simpleArraySum(int[] ar) {
+    static int simpleArraySum(int[] ar)
+   {
         int add=0;
         
         for(int i=0;i<ar.length;i++)
@@ -25,7 +24,8 @@ public class Solution {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException 
+    {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int arCount = Integer.parseInt(scanner.nextLine().trim());
