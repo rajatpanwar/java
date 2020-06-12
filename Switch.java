30ac370f797db9ed25183589e882821b8e98d874
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-12 23:12:03 +0530

    second commit

--- Switch.java
+++ Switch.java
@@ -4,3 +4,13 @@ public static void main(String[] args)
 {
 String s="Hello";
 switch(s)
+  {
+case "hi":
+System.out.println("hi");
+break;
+case "hello":
+System.out.println("hello");
+break;
+}
+}
+}
