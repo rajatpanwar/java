d03d317f09525e35684ba86010efb0dcaf2a436b
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-12 23:14:47 +0530

    first commit

---
+++ Loop.java
@@ -0,0 +1,16 @@
+public class loop
+{
+public static void main(String[] args)
+{
+int x=1;
+while(x<10)
+{
+System.out.println(x);
+x++;
+}
+boolean a=true;
+while(a)
+{
+System.out.println(a);
+a++;
+}
