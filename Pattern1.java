1d1856e44186b252453f51bbdf29f1926943b039
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-13 22:50:37 +0530

    third commit

--- Pattern1.java
+++ Pattern1.java
@@ -2,7 +2,7 @@ public class Pattern1
 {
 public static void main(String[] args)
 {
-for(int a=1;a<=4;a++)
+for(int a=1;a<=5;a++)
 {
 for(int b=1;b<=i;b++)
 {
@@ -12,3 +12,10 @@ System.out.println(" ");
 }
 }
 }
+
+//output-
+*
+* *
+* * *
+* * * *
+* * * * *
