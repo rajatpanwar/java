91a68870710a122a027b769093944e2ff6f6a8ff
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-14 22:28:22 +0530

    after comment

--- Pattern3.java
+++ Pattern3.java
@@ -2,11 +2,11 @@ public class Pattern3
 {
 public static void main(String[] args)
 {
-for(int i=1;i<=5;i++)
+for(int i=1;i<=5;i++)     //number of rows
 {
-for(int j=1;j<=i;j++)
+for(int j=1;j<=i;j++)     //number of columns
 {
-if((i+j)%2==0)
+if((i+j)%2==0)                  //figure out the positions where we need to print 1 and 0
 System.out.print("1");
 else
 System.out.print("0");
