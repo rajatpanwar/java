4267e5289ce77a7936b1e39a9e553aa183d47db0
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-14 22:39:29 +0530

    after comment

--- Pattern4.java
+++ Pattern4.java
@@ -2,11 +2,11 @@ public class Pattern4
 {
 public static void main(String[] args)
 {
-for(int i=1;i<=5;i++)       //number of rows
+for(int a=1;a<=5;a++)       //number of rows
 {
-for(int j=1;j<=i;j++)       //number of columns
+for(int b=1;b<=a;b++)       //number of columns
 {
-System.out.print(i+" ");
+System.out.print(a+" ");
 }
 System.out.println(" ");
 }
