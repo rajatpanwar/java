a7d82da771f7cfd78efc3026ee03ae02fa553975
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-15 22:48:52 +0530

    third commit

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,18 +1,18 @@
 public class Fibonacci
 {
-public static void main(String[] args)
-{
-int a,b,c;
-a=0;
-b=1;
-c=1;
-System.out.println("1 1");
-while(a<=50)
-{
-a=b+c;
-System.out.println(a+" ");
-b=c;
-c=a;
-}
-}
+  public static void main(String[] args)
+  {
+    int a,b,c;
+    a=0;
+    b=1;
+    c=1;
+    System.out.println("1 1");
+    while(a<=50)
+    {
+      a=b+c;
+      System.out.println(a+" ");
+      b=c;
+      c=a;
+    }
+  }
 }
