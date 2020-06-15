4280514e0bf7a1bd261d3aae64df356fc7796fd1
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-15 22:52:56 +0530

    after comment

--- Ascii.java
+++ Ascii.java
@@ -1,8 +1,8 @@
 public class Ascii
 {
-  public static void main(String[] args)
+  public static void main(String[] args)       
   {
-    for(int x=0; x<=127; x++)
+    for(int x=0; x<=127; x++)                     //there are 127 characters and every one has their own ascii values
     {
      System.out.printf("%d : %c \n",x,x);
     }
