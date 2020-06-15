6f5348fcc9a871a0850a140ac1bc5feba080ea00
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-15 23:05:11 +0530

    after comment

--- Pattern4.java
+++ Pattern4.java
@@ -1,16 +1,16 @@
 public class Pattern4
 {
-public static void main(String[] args)
-{
-for(int a=1;a<=5;a++)       //number of rows
-{
-for(int b=1;b<=a;b++)       //number of columns
-{
-System.out.print(a+" ");
-}
-System.out.println(" ");
-}
-}
+  public static void main(String[] args)
+  {
+   for(int a=1;a<=5;a++)       //number of rows
+   {
+    for(int b=1;b<=a;b++)       //number of columns
+    {
+     System.out.print(a+" ");
+    }
+    System.out.println(" ");
+   }
+ }
 }
 
 
