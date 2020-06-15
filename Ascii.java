c71c0c337696104a00bebde6713858483aeabe2f
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-15 22:51:49 +0530

    third commit

--- Ascii.java
+++ Ascii.java
@@ -1,10 +1,10 @@
 public class Ascii
 {
-public static void main(String[] args)
-{
-for(int x=0; x<=127; x++)
-{
-System.out.printf("%d : %c \n",x,x);
-}
-}
+  public static void main(String[] args)
+  {
+    for(int x=0; x<=127; x++)
+    {
+     System.out.printf("%d : %c \n",x,x);
+    }
+  }
 }
