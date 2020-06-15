9c9a743ace193ad2ea71008f787db642d82e37a9
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-06-15 22:54:09 +0530

    third commit

--- If_else.java
+++ If_else.java
@@ -1,17 +1,17 @@
 public class If_else
 {
-public static void main(String[] args)
-{
-int x,y,z;
-x=10;
-y=5;
-z=15;
-if(x>y && x>z)
-  System.out.println(x);
-else if(y>z)
-  System.out.println(y);
- else
-   System.out.println(z);
-}
+  public static void main(String[] args)
+  {
+   int x,y,z;
+   x=10;
+   y=5;
+   z=15;
+   if(x>y && x>z)
+     System.out.println(x);
+   else if(y>z)
+     System.out.println(y);
+   else
+     System.out.println(z);
+   }
 }
 
