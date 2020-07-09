98ee8f5f98f6f2dfe5a315c17e83dd6fa09aab9c
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-09 23:46:47 +0530

    after comment

--- STRING_BUFFER/Demo1.java
+++ STRING_BUFFER/Demo1.java
@@ -3,7 +3,7 @@ public class Demo1
     public static void main(String[] args)
     {
       StringBuffer sb =new StringBuffer(" Rajat ");
-       sb.insert(7,"Panwar");
+       sb.insert(7,"Panwar");                                //here is 7 is index number where we can put String panwar
        System.out.println(sb);
      }
 }
