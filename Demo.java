b97edb9eeb9ee109c999d8ff56d7911edea1e5e6
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-09 23:26:37 +0530

    after 2nd comment

--- Demo.java
+++ Demo.java
@@ -4,7 +4,11 @@ public class Demo
     {
         StringBuffer sb=new StringBuffer("I READ IN UPES");
            System.out.println(sb);
-            sb.append("UPES IS THE FUTURE OF STUDENT");
+            sb.append("UPES IS THE FUTURE OF STUDENT");     //modifying the object
          System.out.println(sb);
        }
 }
+
+
+//Output is --------     I READ IN UPES
+// --------------------   I READ IN UPES UPES IS THE FUTURE OF STUDENT
