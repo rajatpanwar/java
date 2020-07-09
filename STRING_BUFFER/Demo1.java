8dc5547c778699b2e16bff3c6b4d3f5848577c61
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-09 23:45:07 +0530

    insert the value with the help of insert function

---
+++ STRING_BUFFER/Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1
+{
+    public static void main(String[] args)
+    {
+      StringBuffer sb =new StringBuffer(" Rajat ");
+       sb.insert(7,"Panwar");
+       System.out.println(sb);
+     }
+}
