8b8964f3835ce63fb1c4e268feb219c4f629de08
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-09 23:23:47 +0530

    creating a string buffer object

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo
+{
+   public static void main(String[] args)
+    {
+        StringBuffer sb=new StringBuffer("I READ IN UPES");
+           System.out.println(sb);
+            sb.append("UPES IS THE FUTURE OF STUDENT");
+         System.out.println(sb);
+       }
+}
