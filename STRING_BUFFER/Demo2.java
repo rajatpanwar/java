0d002c1fcd6ec7d08ba970a3f9b27ed7bf7c0c73
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-09 23:52:33 +0530

    reverse the string

---
+++ STRING_BUFFER/Demo2.java
@@ -0,0 +1,9 @@
+public class Demo2
+{
+    public static void main(String[] args)
+    {
+      StringBuffer sb =new StringBuffer(" Rajat ");
+       sb.reverse();
+       System.out.println(sb);
+     }
+}
