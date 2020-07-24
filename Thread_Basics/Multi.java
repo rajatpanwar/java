2191217a8e0fe3814dcb0aa6aabc4d8fdb3f86d5
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-24 22:01:40 +0530

    after add 2nd code

--- Thread_Basics/Multi.java
+++ Thread_Basics/Multi.java
@@ -7,3 +7,18 @@ Multi t1=new Multi();
 t1.start();  
  }  
 }  
+
+
+<<---------- a thread can never start again thread run only aone time . it will throw exception second time ---------->>>>
+
+  class Multi extends Thread{  
+public void run(){  
+System.out.println("thread is running...");  
+}  
+public static void main(String args[]){  
+Multi t1=new Multi();  
+t1.start();
+t1.start();
+ }  
+}  
+
