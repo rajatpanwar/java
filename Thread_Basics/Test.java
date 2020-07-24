3e93dea7e39188a2bfae085e001860da1eb336ed
rajatpanwar <46771479+rajatpanwar@users.noreply.github.com>
2020-07-24 21:51:56 +0530

    after adding some line

--- Thread_Basics/Test.java
+++ Thread_Basics/Test.java
@@ -20,8 +20,9 @@ public static void main(String args[])
 {
     Test t1 = new Test();
     Test t2 = new Test();
-     
+    Test t3 = new Test();
      t1.start();
      t2.start();
+     t3.start();
 }
 }
